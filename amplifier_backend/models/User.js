da0fd966e342da1a6fb9942fe958b95469d25248
Gunadeep <gunadeep19082005@gmail.com>
2024-11-09 10:32:55 +0530

    mongoose

---
+++ amplifier_backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose=reuire("mongoose");
+
+// How to create a model.
+// 1- reuire mongoose.
+// 2- create a mongoose schema.
+// 3- create model.
+
+const User = new mongoose.Schema({
+    firstName: {type: String, required:true,},
+    // type defines data type and required is used that this feild is must for the user to fill without this feild a user cannot exist.
+
+    lastName: {type:String,required:false,},
+    email:{type:String,required:true,},
+    username:{type:String,required:true,},
+    likedSongs:{type:String,default:"",},
+    likedPlaylists:{type:String,default:"",},
+    subscribedArtists:{type:String,default:"",},
+});
+
+const UserModel = mongoose.model("User",User)
+
+module.exports = UserModel;
