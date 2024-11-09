2c2c1b13aa8f18b0f4704649f303e1ead030bcd5
csdeepak <146485921+csdeepak@users.noreply.github.com>
2024-11-09 10:52:45 +0530

    songs_playlist

---
+++ amplifier_backend/models/Playlist.js
@@ -0,0 +1,43 @@
+const mongoose=reuire("mongoose");
+
+// How to create a model.
+// 1- reuire mongoose.
+// 2- create a mongoose schema.
+// 3- create model.
+
+const Playlist = new mongoose.Schema({
+    name :{
+        type: String,
+        required: true,
+    },
+    thumbnail :{
+        type: String,
+        required: true,
+    },
+    
+    owner:{
+        type: mongoose.Type.ObjectId,
+        ref: "user",
+    },
+    //playlist songs 
+    //playlist collaborators
+
+    songs:[
+        {
+        type: mongoose.Type.ObjectId,
+        ref :"song"
+        
+    },
+    ],
+    collaborators :{
+        type: mongoose.Type.ObjectId,
+        ref: "user",
+    },
+
+
+    
+});
+
+const PlaylistModel = mongoose.model("Playlist",Playlist)
+
+module.exports = PlaylistModel;
---
+++ amplifier_backend/models/Song.js
@@ -0,0 +1,30 @@
+const mongoose=reuire("mongoose");
+
+// How to create a model.
+// 1- reuire mongoose.
+// 2- create a mongoose schema.
+// 3- create model.
+
+const Song = new mongoose.Schema({
+    name :{
+        type: String,
+        required: true,
+    },
+    thumbnail :{
+        type: String,
+        required: true,
+    },
+    track :{
+        type: String,
+        required: true,
+    },
+    artist :{
+        type: mongoose.Type.ObjectId,
+        ref: "user",
+    }
+    
+});
+
+const SongModel = mongoose.model("Song",Song)
+
+module.exports = SongModel;
