4ce6e8513da0b69ef8fadb783977d6b363735a59
Gunadeep <gunadeep19082005@gmail.com>
2024-11-10 08:37:49 +0530

    changes

--- amplifier_backend/routes/song.js
+++ amplifier_backend/routes/song.js
@@ -9,15 +9,13 @@ const Song=require("../models/Song");
 
 router.post("/create", passport.authenticate("jwt",{session:false}),async(req,res)=>{
     //req.user gets the user beecause of the passport.authenticate
-    const{name,thumbnail,track}=req.body;
+    const {name,thumbnail,track} = req.body;
     if(!name||!thumbnail||!track){
-        return res
-            .status(301)
-            .json({err:"Insufficient details to creates song."});
+        return res.status(301).json({err:"Insufficient details to creates song."});
     }
     const artist =req.user._id;
     const songDetails={name,thumbnail,track,artist};
-    const createdSong =await SongModel.create(songDetails);
+    const createdSong =await Song.create(songDetails);
     return res.status(200).json(createdSong)
 
 });
@@ -25,7 +23,7 @@ router.post("/create", passport.authenticate("jwt",{session:false}),async(req,re
 // get route to get all song that i ahve published
 router.get("/get/mysongs",passport.authenticate("jwt",{session:false}),async(req, res)=>{
     const currentUser=req.user;
-    const songs=await Song.find({artist:req.user._id});
+    const songs=await Song.find({artist: req.user._id});
     return res.status(200).json({data:songs});
 });
 
