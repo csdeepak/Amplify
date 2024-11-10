ffe47ef35e6ff35057993e2589f4222bc62c6ea5
csdeepak <146485921+csdeepak@users.noreply.github.com>
2024-11-10 18:01:31 +0530

    17_18

--- amplifier_frontend/src/App.js
+++ amplifier_frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        {
+          /* adding routes components here indicates to the package{react-router-dom} 
+          that  we are starting  to define our routes inside this */
+        }
+          <Route path = "/" element={<HelloComponent/>}/>
+          <Route path = "/hi" element={<div>hi</div>}/>
+          <Route path = "/bye" element={<div>bye</div>}/>
+
+
+        
+      </Routes>
+      </BrowserRouter>
+
   );
 }
+const HelloComponent=()=>{
+  return <div>this is hello from component</div>
+};
 
 export default App;
