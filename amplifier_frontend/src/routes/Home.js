441277bc72266b41a3c604bb9d4c9b9ce6a14664
Gunadeep <gunadeep19082005@gmail.com>
2024-11-11 22:17:18 +0530

    making_cards_28_29

--- amplifier_frontend/src/routes/Home.js
+++ amplifier_frontend/src/routes/Home.js
@@ -2,6 +2,28 @@ import spotify_logo from "../assets/images/spotify_logo_white.svg";
 import IconText from "../components/shared/IconText";
 import {Icon} from "@iconify/react"
 import TextWithHover from "../components/shared/TextWithHover";
+
+const focusCardsData =[{title:"Peaceful Piano",description:"Relax and Indulge with Beautiful Piano Pieces",imgUrl:"https://images.unsplash.com/photo-1577877777751-3f1ec20a0715?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMHBsYXlpbmclMjBwaWFub3xlbnwwfHwwfHx8MA%3D%3D"},
+                       {title:"Deep Focus", description:"Keep Calm And Focus with Music",imgUrl:"https://images.unsplash.com/photo-1453748866136-b1dd97284f49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHN0dWR5fGVufDB8fDB8fHww"},
+                       {title:"Instrumental Study",description:"Focus With Soft Study Music",imgUrl:"https://media.istockphoto.com/id/517324559/photo/violin-in-vintage-style.webp?a=1&b=1&s=612x612&w=0&k=20&c=oxV1IILse0nDB-ukZlA2OSGVc3XgcBhUqJt597lTDtw="},
+                       {title:"Focus Flow",description:"Up tempo Instrumental Hip Hop Beats",imgUrl:"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvcmt8ZW58MHx8MHx8fDA%3D" },
+                       {title:"Beats to Think to",description:"Focus With Deep Techno And Tch House",imgUrl:"https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vdGJhbGwlMjBwbGF5ZXJ8ZW58MHx8MHx8fDA%3D"},
+                      ];
+
+const spotifyPalylistsCardData =[{title:"Hello",description:"Relax and Indulge with Beautiful Piano Pieces",imgUrl:"https://images.unsplash.com/photo-1577877777751-3f1ec20a0715?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMHBsYXlpbmclMjBwaWFub3xlbnwwfHwwfHx8MA%3D%3D"},
+                       {title:"Deep Focus", description:"Keep Calm And Focus with Music",imgUrl:"https://images.unsplash.com/photo-1453748866136-b1dd97284f49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHN0dWR5fGVufDB8fDB8fHww"},
+                       {title:"Instrumental Study",description:"Focus With Soft Study Music",imgUrl:"https://media.istockphoto.com/id/517324559/photo/violin-in-vintage-style.webp?a=1&b=1&s=612x612&w=0&k=20&c=oxV1IILse0nDB-ukZlA2OSGVc3XgcBhUqJt597lTDtw="},
+                       {title:"Focus Flow",description:"Up tempo Instrumental Hip Hop Beats",imgUrl:"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvcmt8ZW58MHx8MHx8fDA%3D" },
+                       {title:"Beats to Think to",description:"Focus With Deep Techno And Tch House",imgUrl:"https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vdGJhbGwlMjBwbGF5ZXJ8ZW58MHx8MHx8fDA%3D"},
+                       ];
+
+const soundofIndiaCardsData =[{title:"sound",description:"Relax and Indulge with Beautiful Piano Pieces",imgUrl:"https://images.unsplash.com/photo-1577877777751-3f1ec20a0715?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMHBsYXlpbmclMjBwaWFub3xlbnwwfHwwfHx8MA%3D%3D"},
+                       {title:"Deep Focus", description:"Keep Calm And Focus with Music",imgUrl:"https://images.unsplash.com/photo-1453748866136-b1dd97284f49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHN0dWR5fGVufDB8fDB8fHww"},
+                       {title:"Instrumental Study",description:"Focus With Soft Study Music",imgUrl:"https://media.istockphoto.com/id/517324559/photo/violin-in-vintage-style.webp?a=1&b=1&s=612x612&w=0&k=20&c=oxV1IILse0nDB-ukZlA2OSGVc3XgcBhUqJt597lTDtw="},
+                       {title:"Focus Flow",description:"Up tempo Instrumental Hip Hop Beats",imgUrl:"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvcmt8ZW58MHx8MHx8fDA%3D" },
+                       {title:"Beats to Think to",description:"Focus With Deep Techno And Tch House",imgUrl:"https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vdGJhbGwlMjBwbGF5ZXJ8ZW58MHx8MHx8fDA%3D"},
+                       ];
+
 const Home = () =>{
     return( 
     <div className="h-full w-full flex">
@@ -35,7 +57,7 @@ const Home = () =>{
 
         {/*the second div tag is for main content*/}
 
-        <div className="h-full w-4/5 bg-app-black">
+        <div className="h-full w-4/5 bg-app-black overflow-auto">
             <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
                 <div className="w-1/2 flex h-full">
                     <div className="w-3/5 flex justify-around items-center">
@@ -54,46 +76,58 @@ const Home = () =>{
                     </div>
                 </div>
             </div>
-            <div className="content p-8">
-                <PlaylistView />
+            <div className="content p-8 pt-0 overflow-auto">
+                <PlaylistView titleText="Focus" cardsData={focusCardsData}/>
+                <PlaylistView titleText="Spotify Playlist" cardsData={spotifyPalylistsCardData}/>
+                <PlaylistView titleText="Sound of India" cardsData={soundofIndiaCardsData}/>
             </div>
         </div>
     </div>
     );
 };
 
-const PlaylistView = () =>{
+const PlaylistView = ({titleText,cardsData}) =>{
     return (
-        <div className="text-white">
+        <div className="text-white mt-8">
             <div className="text-2xl font-semibold mb-5">
-                Focus
+                {titleText}
             </div>
             <div className="w-full flex justify-between space-x-4">
-                <Card 
+                {
+                    //cardsData will be an array.
+                    cardsData.map(items=>{
+                        return (<Card 
+                        title={items.title} 
+                        description={items.description} 
+                        imgUrl={items.imgUrl}/>
+                        );
+                    })
+                }
+                {/*<Card 
                     title="Peaceful Piano" 
                     description="Relax and Indulge with Beautiful Piano Pieces"
-                    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDE4cJvMUaRNtQKS6pJCi7je2_72uwO5USw&s"
+                    imgUrl="https://images.unsplash.com/photo-1577877777751-3f1ec20a0715?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMHBsYXlpbmclMjBwaWFub3xlbnwwfHwwfHx8MA%3D%3D"
                 />
                 <Card 
                     title="Deep Focus " 
                     description="Keep Calm And Focus with Music"
-                    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDE4cJvMUaRNtQKS6pJCi7je2_72uwO5USw&s"
+                    imgUrl="https://images.unsplash.com/photo-1453748866136-b1dd97284f49?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHN0dWR5fGVufDB8fDB8fHww"
                 />
                 <Card 
                     title="Instrumental Study "
                     description="Focus With Soft Study Music"
-                    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDE4cJvMUaRNtQKS6pJCi7je2_72uwO5USw&s"
+                    imgUrl="https://media.istockphoto.com/id/517324559/photo/violin-in-vintage-style.webp?a=1&b=1&s=612x612&w=0&k=20&c=oxV1IILse0nDB-ukZlA2OSGVc3XgcBhUqJt597lTDtw="
                 />
                 <Card 
                     title="Focus Flow"
                     description="Up tempo Instrumental Hip Hop Beats"
-                    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDE4cJvMUaRNtQKS6pJCi7je2_72uwO5USw&s" 
+                    imgUrl="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvcmt8ZW58MHx8MHx8fDA%3D" 
                 />
                 <Card 
                     title="Beats to Think to"
                     description="Focus With Deep Techno And Tch House"
-                    imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQDE4cJvMUaRNtQKS6pJCi7je2_72uwO5USw&s"
-                />
+                    imgUrl="https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vdGJhbGwlMjBwbGF5ZXJ8ZW58MHx8MHx8fDA%3D"
+                />*/}
             </div>
         </div>
     );
@@ -101,7 +135,7 @@ const PlaylistView = () =>{
 
 const Card = ({title,description,imgUrl}) =>{
     return(
-        <div className="bg-black bg-opacity- w-1/5 p-4  rounded-lg">
+        <div className="bg-black bg-opacity-40 w-1/5 p-4  rounded-lg">
             <div className="pb-4 pt-2">
                 <img className="w-full rounded-md"
                 src={imgUrl}
---
+++ amplifier_frontend/src/utils/serverHelpers.js
@@ -0,0 +1,14 @@
+import {backendUrl} from "./config";
+
+export const makeUnauthenticatedPOSTRequest = async(route,body) => {
+    // route : /signup
+    const response = await fetch(backendUrl+route, {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(body), 
+    });
+    const formattedResponse = await response.json();
+    return formattedResponse;
+};
