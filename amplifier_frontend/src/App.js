a814650d3044299e65a70c1e2530ed0b6cd4b8f6
csdeepak <146485921+csdeepak@users.noreply.github.com>
2024-11-15 10:22:44 +0530

    deepak

--- amplifier_frontend/src/App.js
+++ amplifier_frontend/src/App.js
@@ -1,5 +1,6 @@
 // 22nd for font-poppins.
 import "./output.css";
+import { useState } from "react";
 import {BrowserRouter, Routes, Route,Navigate} from "react-router-dom";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
@@ -8,8 +9,10 @@ import {useCookies} from "react-cookie";
 import LoggedInHomeComponent from "./routes/LoggedInHome";
 import UploadSong from "./routes/UploadSong";
 import MyMusiuc from "./routes/MyMusic";
+import songContext from "./contexts/songContext";
 
 function App() {
+  const [currentSong,setCurrentSong]=useState(null);
   const [cookie,setCookie] = useCookies(["token"]);
 
 
@@ -18,15 +21,17 @@ function App() {
       <BrowserRouter>
       {
         cookie.token? (
-        <Routes>
-          { /* adding routes components here indicates to the package{react-router-dom} that  we are starting  to define our routes inside this */}
-            <Route path = "/" element={<HelloComponent/>}/>
-            <Route path="/home"  element={<LoggedInHomeComponent/>}/>
-            <Route path="/uploadSong" element={<UploadSong/>}/>
-            <Route path="/myMusic" element={<MyMusiuc/>}/>
-            <Route path="*" element={<Navigate to="/home"/>}/> {/*  " * " in path it represents if i go to any other send me to home page. */}
-
+        <songContext.Provider value={{currentSong,setCurrentSong}}>
+           <Routes>
+                { /* adding routes components here indicates to the package{react-router-dom} that  we are starting  to define our routes inside this */}
+                <Route path = "/" element={<HelloComponent/>}/>
+                <Route path="/home"  element={<LoggedInHomeComponent/>}/>
+                <Route path="/uploadSong" element={<UploadSong/>}/>
+                <Route path="/myMusic" element={<MyMusiuc/>}/>
+                <Route path="*" element={<Navigate to="/home"/>}/> {/*  " * " in path it represents if i go to any other send me to home page. */}
+            
           </Routes>
+        </songContext.Provider>
         ): (
             <Routes>
               <Route path="/home"  element={<HomeComponent/>}/>
--- amplifier_frontend/src/routes/LoggedInHome.js
+++ amplifier_frontend/src/routes/LoggedInHome.js
@@ -32,65 +32,70 @@ const EnglishAlbumCardsData =[{title:"Justin Bieber",description:"Artist",imgUrl
 
 const Home = () =>{
     return( 
-    <div className="h-full w-full flex">
-        {/*this first  div tag is for left pannel*/}
-        <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
-            <div>
-                {/*its for logo*/}
-                <div className="logoDiv p-6">
-                    <img src={spotify_logo} alt="spotify logo" width={125}/>
+    <div className="h-full w-full bg-black">
+        <div className="h-9/10 w-full flex">
+            {/*this first  div tag is for left pannel*/}
+            <div className="h-full w-1/5 bg-black flex flex-col justify-between pb-10">
+                <div>
+                    {/*its for logo*/}
+                    <div className="logoDiv p-6">
+                        <img src={spotify_logo} alt="spotify logo" width={125}/>
 
-                </div>
-                <div className="py-5">
-                    <IconText iconName={"material-symbols:home"} displayText={"Home"} active/>
-                    <IconText iconName={"ic:round-search"} displayText={"Search"}/>
-                    <IconText iconName={"mdi:bookshelf"} displayText={"Library"}/>
-                    <IconText iconName={"fxemoji:musicascend"} displayText={"My Songs"}/>
-                </div>
-                <div className="pt-5">
-                <IconText iconName={"material-symbols:add-box"} displayText={"Create Playlist"}/>
-                <IconText iconName={"fluent-emoji-flat:red-heart"} displayText={"Liked Songs"}/>
-                </div>
-            </div>
-            <div className="px-1">
-                <div className="border border-gray-100 text-white w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer">
-                    <Icon icon="flat-color-icons:globe"/>
-                    <div className="ml-2 text-sm font-semibold">
-                        English
-                    </div>    
-                </div>
-            </div>
-        </div>
-
-        {/*the second div tag is for main content*/}
-
-        <div className="h-full w-4/5 bg-app-black overflow-auto">
-            <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
-                <div className="w-1/2 flex h-full">
-                    <div className="w-2/3 flex justify-around items-center text-sm">
-                        <TextWithHover displayText={"Premium"}/>
-                        <TextWithHover displayText={"Support"}/>
-                        <TextWithHover displayText={"Download"}/>
-                        <div className="h-1/2 border-r border-white">
-
-                        </div>
                     </div>
-                    <div className="w-1/3 flex justify-around h-full items-center">
-                        <TextWithHover displayText={"Upload Song"}/>
-                        <div className="bg-white w-10 h-10 flex items-center justify-center rounded-full font-semibold cursor-pointer">
-                            AC
-                        </div>
+                    <div className="py-5">
+                        <IconText iconName={"material-symbols:home"} displayText={"Home"} active/>
+                        <IconText iconName={"ic:round-search"} displayText={"Search"}/>
+                        <IconText iconName={"mdi:bookshelf"} displayText={"Library"}/>
+                        <IconText iconName={"fxemoji:musicascend"} displayText={"My Songs"}/>
+                    </div>
+                    <div className="pt-5">
+                    <IconText iconName={"material-symbols:add-box"} displayText={"Create Playlist"}/>
+                    <IconText iconName={"fluent-emoji-flat:red-heart"} displayText={"Liked Songs"}/>
+                    </div>
+                </div>
+                <div className="px-1">
+                    <div className="border border-gray-100 text-white w-2/5 flex px-2 py-1 rounded-full items-center justify-center hover:border-white cursor-pointer">
+                        <Icon icon="flat-color-icons:globe"/>
+                        <div className="ml-2 text-sm font-semibold">
+                            English
+                        </div>    
                     </div>
                 </div>
             </div>
-            <div className="content p-8 pt-0 overflow-auto">
-                <PlaylistView titleText="Focus" cardsData={focusCardsData}/>
-                <PlaylistView titleText="Spotify Playlist" cardsData={spotifyPalylistsCardData}/>
-                <PlaylistView titleText="Telugu Album" cardsData={TeluguAlbumCardsData}/>
-                <PlaylistView titleText="English Album" cardsData={EnglishAlbumCardsData}/>
+
+            {/*the second div tag is for main content*/}
+
+            <div className="h-full w-4/5 bg-app-black overflow-auto">
+                <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
+                    <div className="w-1/2 flex h-full">
+                        <div className="w-2/3 flex justify-around items-center text-sm">
+                            <TextWithHover displayText={"Premium"}/>
+                            <TextWithHover displayText={"Support"}/>
+                            <TextWithHover displayText={"Download"}/>
+                            <div className="h-1/2 border-r border-white">
+
+                            </div>
+                        </div>
+                        <div className="w-1/3 flex justify-around h-full items-center">
+                            <TextWithHover displayText={"Upload Song"}/>
+                            <div className="bg-white w-10 h-10 flex items-center justify-center rounded-full font-semibold cursor-pointer">
+                                AC
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="content p-8 pt-0 overflow-auto">
+                    <PlaylistView titleText="Focus" cardsData={focusCardsData}/>
+                    <PlaylistView titleText="Spotify Playlist" cardsData={spotifyPalylistsCardData}/>
+                    <PlaylistView titleText="Telugu Album" cardsData={TeluguAlbumCardsData}/>
+                    <PlaylistView titleText="English Album" cardsData={EnglishAlbumCardsData}/>
+                </div>
             </div>
-        </div>
-    </div>
+        </div> 
+        <div className="w-full h-1/20 bg-black bg-opacity-30 ">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSWDO6rpgwFGEvAU3GLkzwrkHplgXh8yLT9w&s"/>
+        </div>   
+    </div>      
     );
 };
 
