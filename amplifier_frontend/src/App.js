fb4cce3fd3dedb841c430245ab5dd72b122d5f0a
Gunadeep <gunadeep19082005@gmail.com>
2024-11-19 10:11:13 +0530

    linking_signup_login_to_home

--- amplifier_frontend/src/App.js
+++ amplifier_frontend/src/App.js
@@ -41,10 +41,9 @@ function App() {
         </songContext.Provider>
         ): (
             <Routes>
-              
+              <Route path="/home"  element={<HomeComponent/>}/>
               <Route path = "/login" element={<LoginComponent/>}/>
               <Route path ="/signup" element ={<SignupComponent/>}/>
-              <Route path="/home"  element={<HomeComponent/>}/>
               <Route path="*" element={<Navigate to="/login"/>}/>
             </Routes>
 
